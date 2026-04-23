8b6bf7f8183d600c01e079878b257f5099ae98f4
Juan Diego <juandbp@educastur.org>
2026-04-23 20:05:38 +0200

    Primera versión

---
+++ src/main/java/com/dam/calculatorJBP/CalculatorJbpApplication.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorJBP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorJbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorJbpApplication.class, args);
+	}
+
+}
